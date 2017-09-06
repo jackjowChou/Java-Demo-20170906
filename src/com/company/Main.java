11ea0c72fcc669a88de6d494a7a70fe975aa6ef9
yungchenjow <yungchen@asia.edu.tw>
2017-09-06 14:11:42 +0800

    commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
 	    System.out.println("hello");
+	    int sum = 0;
+	    for(int i= 0;i <=n; i++){
+	        sum += i;
+        }
     }
 }
